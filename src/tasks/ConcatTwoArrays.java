403804a9c2b866091da35f66d8aed684f57ae5c8
sinanguzel <33018094+sinanguzel@users.noreply.github.com>
2022-06-14 15:58:03 -0400

    java tasks

--- src/tasks/ConcatTwoArrays.java
+++ src/tasks/ConcatTwoArrays.java
@@ -1,2 +1,34 @@
-package tasks;public class ConcatTwoArrays {
+package tasks;
+
+import java.util.Arrays;
+
+public class ConcatTwoArrays {
+    public static void main(String[] args) {
+
+        int[] arr1 = {12, 24, 654, 66, 33};
+        int[] arr2 = {23, 45, 77, 9, 88};
+
+
+
+        System.out.println(Arrays.toString(concatTwoArrays(arr1, arr2)));
+    }
+
+    public static int[] concatTwoArrays(int[] arr1, int[] arr2) {
+        int [] result = new int[arr1.length + arr2.length];
+        int i = 0;
+
+        for (int each : arr1) {
+            result[i] = each;
+            i++;
+        }
+
+        for (int each : arr2) {
+            result[i] = each;
+            i++;
+        }
+
+        return result;
+
+
+    }
 }
--- src/tasks/FrequencyOfCharacters.java
+++ src/tasks/FrequencyOfCharacters.java
@@ -1,2 +1,31 @@
-package tasks;public class FrequencyOfCharacters {
+package tasks;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FrequencyOfCharacters {
+    public static void main(String[] args) {
+        //write a method that prints the frequency of each character from a string
+
+        String str = "abcsdqdqwq";
+
+        frequencyTest(str);
+
+    }
+    public static void frequencyTest(String str){
+        //linkedHashmap maintains the order
+        Map<Character,Integer> map = new LinkedHashMap<>();
+
+        for (Character each:str.toCharArray()) {
+            if(map.containsKey(each)){
+                map.put(each,map.get(each)+1);
+            }else{
+                map.put(each,1);
+            }
+
+        }
+
+        System.out.println(map);
+
+    }
 }

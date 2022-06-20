d745091a22808c0be14faa6083f1ebeedf7ad14d
sinanguzel <33018094+sinanguzel@users.noreply.github.com>
2022-06-20 10:58:52 -0400

    return only hours task

---
+++ src/tasks/ReturnOnlyHours.java
@@ -0,0 +1,23 @@
+package tasks;
+
+import java.util.Arrays;
+
+public class ReturnOnlyHours {
+    public static void main(String[] args) {
+        //given a time return only hours
+
+        //11:00, 9:0, 15:00
+        String str = "24:00";
+
+
+        System.out.println(returnHours(str));
+
+    }
+
+    public static String returnHours(String time){
+
+        String[]arr = time.split(":");
+
+        return arr[0];
+    }
+}
